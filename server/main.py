5b97603d518d38ae30a54aa3d1296d0417a0dd71
Alec Elton <alec.elton@gmail.com>
2019-10-27 15:00:31 -0500

    Accept args

--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
-# import time
-# import signal
+#!/usr/bin/env python3
+
+import argparse
 
 from app.lib.config import parse_config
 from app.inputs import DeviceInput
@@ -10,34 +11,45 @@ from app.outputs.gui import GUI
 from app.outputs.dmx import DMX
 
 
-config = parse_config()
-tasks = [
-    DeviceInput('audioinput', config),
-    SmoothingProcessor('smoothing', config),
-    BeatProcessor('beat', config),
-    IdleProcessor('idle', config),
-]
-for output in config['OUTPUTS']:
-    tasks.append(globals()[output['DEVICE']](config, output))
-if config['USE_GUI']:
-    tasks.append(GUI('gui', config))
-# Add DMX last so it gets called last
-if config.get('DMX_DEVICE'):
-    tasks.append(DMX('dmx', config))
+def parse_args():
+    p = argparse.ArgumentParser(description="Audio reactive lights server")
+    p.add_argument('-c', '--config', help="Configuration file")
+    return p.parse_args()
 
-try:
+
+def run(args):
+    config = parse_config(filename=args.config)
+    tasks = [
+        DeviceInput('audioinput', config),
+        SmoothingProcessor('smoothing', config),
+        BeatProcessor('beat', config),
+        IdleProcessor('idle', config),
+    ]
+    for output in config['OUTPUTS']:
+        tasks.append(globals()[output['DEVICE']](config, output))
+    if config['USE_GUI']:
+        tasks.append(GUI('gui', config))
+    # Add DMX last so it gets called last
+    if config.get('DMX_DEVICE'):
+        tasks.append(DMX('dmx', config))
+
     try:
-        data = {}
-        for t in tasks:
-            t.start(data)
-
-        while True:
+        try:
             data = {}
             for t in tasks:
-                t.run(data)
-    except KeyboardInterrupt:
-        # Suppress
-        pass
-finally:
-    for t in tasks:
-        t.stop()
+                t.start(data)
+
+            while True:
+                data = {}
+                for t in tasks:
+                    t.run(data)
+        except KeyboardInterrupt:
+            # Suppress
+            pass
+    finally:
+        for t in tasks:
+            t.stop()
+
+
+if __name__ == '__main__':
+    run(parse_args())
