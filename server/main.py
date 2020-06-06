43016731a08944ee960ff420238c493870812d75
Alec Elton <alec.elton@gmail.com>
2020-06-06 11:49:14 -0500

    Watchdog timer

--- server/main.py
+++ server/main.py
@@ -2,7 +2,10 @@
 
 import argparse
 import logging
+import multiprocessing
+import time
 import signal
+import os
 
 from app import NoData
 from app.lib.config import parse_config
@@ -29,6 +32,7 @@ def parse_args():
     p.add_argument('-r', '--record', help="Record states to this file")
     p.add_argument('-M', '--monitor', action='store_true', help="Print monitor data (except audio) to the console")
     p.add_argument('-f', '--filter', action='append', help="Filter the monitor data")
+    p.add_argument('-w', '--watchdog', nargs='?', type=float, default=0, help="Watchdog - if the process hangs for this many seconds, restart it")
     return p.parse_args()
 
 
@@ -40,7 +44,7 @@ def install_sighandler():
     signal.signal(signal.SIGTERM, _sig_handler)
 
 
-def run(args):
+def run(args, watchdog=None):
     config = parse_config(filename=args.config)
     lights = []
     config['ENABLE_LINKS'] = True
@@ -72,8 +76,6 @@ def run(args):
     # Add DMX last so it gets called last
     if config.get('DMX_DEVICE'):
         tasks.append(DMX('dmx', config))
-    if config.get('DMXNET_ESP_NODE'):
-        tasks.append(DMXNet('dmxnet', config))
 
     network = Network(config, lights, monitor=args.monitor, monitor_filter=args.filter)
     tasks.insert(0, NetworkTask('netinput', config, network, 'input'))
@@ -93,11 +95,52 @@ def run(args):
                 except NoData:
                     break
 
+            if watchdog:
+                watchdog.value = time.time()
+
         logger.info("Exiting with signal %d", last_signal)
     finally:
         for t in tasks:
             t.stop()
 
 
+def run_watchdog(args):
+    watchdog = multiprocessing.Value('d', time.time() + 1)
+    pid = os.fork()
+    if pid == 0:
+        run(args, watchdog=watchdog)
+    else:
+        install_sighandler()
+        while last_signal is None:
+            if time.time() - watchdog.value >= args.watchdog:
+                logger.error("Child is not responding, killing")
+                os.kill(pid, signal.SIGKILL)
+                return True
+
+            res = os.waitid(os.P_PID, pid, os.WEXITED | os.WNOHANG)
+            if res is not None:
+                if res.si_code !=0:
+                    logger.error("Child exited with code %d, restarting", res.si_code)
+                    return True
+
+        logger.info("Killing child with signal %d", last_signal)
+        os.kill(pid, last_signal)
+        s = time.time()
+        while time.time() - s < 1:
+            res = os.waitid(os.P_PID, pid, os.WEXITED | os.WNOHANG)
+            if res is not None:
+                break
+        if res is None:
+            logger.error("Killing child")
+            os.kill(pid, signal.SIGKILL)
+
+
 if __name__ == '__main__':
-    run(parse_args())
+    args = parse_args()
+    if args.watchdog:
+        logger.info("Starting up with watchdog timer %fs", args.watchdog)
+        while run_watchdog(args):
+            pass
+    else:
+        logger.info("Starting up with no watchdog timer")
+        run(args)
